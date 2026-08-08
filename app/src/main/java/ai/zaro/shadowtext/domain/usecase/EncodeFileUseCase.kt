9c8bed4e4ce3ca5b790e70610b19ce7798414388
Eilya Rastegarpanah <e.rastegarpanah1@gmail.com>
2026-08-08 18:48:19 +0330

    fix: add operator modifier to EncodeFileUseCase.invoke

--- app/src/main/java/ai/zaro/shadowtext/domain/usecase/EncodeFileUseCase.kt
+++ app/src/main/java/ai/zaro/shadowtext/domain/usecase/EncodeFileUseCase.kt
@@ -5,7 +5,7 @@ import ai.zaro.shadowtext.core.engine.StegoEncoder
 import javax.inject.Inject
 
 class EncodeFileUseCase @Inject constructor(private val stegoEncoder: StegoEncoder) {
-    fun invoke(payload: ByteArray, mimeType: String? = null, fileName: String? = null, carrierText: String): EncodeResult {
+    operator fun invoke(payload: ByteArray, mimeType: String? = null, fileName: String? = null, carrierText: String): EncodeResult {
         return stegoEncoder.encode(payload = payload, mimeType = mimeType, fileName = fileName, carrierText = carrierText)
     }
 }
