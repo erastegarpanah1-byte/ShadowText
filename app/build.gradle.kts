6b249d9f1dc654c27c06cc7c62b087547ca3a82f
Eilya Rastegarpanah <e.rastegarpanah1@gmail.com>
2026-08-08 13:01:32 +0330

    fix: downgrade compose-bom to 2023.10.01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,7 @@ android {
 }
 
 dependencies {
-    val composeBom = platform("androidx.compose:compose-bom:2024.01.00")
+    val composeBom = platform("androidx.compose:compose-bom:2023.10.01")
     implementation(composeBom)
     androidTestImplementation(composeBom)
 
